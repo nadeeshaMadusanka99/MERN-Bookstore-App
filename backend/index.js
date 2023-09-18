2efa8829334437c18c8bd88d9df91b19423e72c5
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2023-09-19 01:14:22 +0530

    add react front end and bind with backend

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,26 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import bookRoutes from "./routes/booksRoute.js";
-
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request bodies
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//Oprion 1: Allow all origins with default or cors(*)
+app.use(cors());
+//Option 2: Allow custom origins
+// app.use(
+//   cors(
+//     {
+//       origin: "http://localhost:3000",
+//       methods: ['GET','POST','PUT','DELETE'],
+//       allowedHeaders: ['Content-Type']
+//     }
+//   )
+// );
 
 app.get("/", (req, res) => {
   console.log(req);
